52953d7c5cbfaf24d1fa4b6284120e4a1570783d
Ayush Gautam <Ayush.Gautam@betsol.com>
2022-04-05 19:26:22 +0530

    init

--- static/todoApp/views/todo_list_view.py
+++ static/todoApp/views/todo_list_view.py
@@ -8,8 +8,6 @@ from static.todoApp.utils.serialize_data import TodoListSerializer
 
 
 class TodoListView(Resource):
-    def before_request(self, *args, **kwargs):
-        return cors.before_request(self, *args, **kwargs)
 
     def get(self):
         serialize_instance = TodoListSerializer(Todo.get_all())
@@ -20,26 +18,26 @@ class TodoListView(Resource):
             return jsonify(data), 200
 
     def post(self):
-        title = request.form.get("title")
+        title = request.form.get("title") or request.form.get("title")
         Todo.create(title)
         # return redirect(url_for("home"))
         return {"message": "successfully added task"}, 201
 
     def put(self):
-        todo_id = request.form.get("todo_id")
-        title = request.form.get("title")
+        todo_id = request.form.get("todo_id") or request.form.get("todo_id")
+        title = request.form.get("title") or request.form.get("title")
         Todo.update(todo_id,title)
         # return redirect(url_for("home"))
         return {"message": "successfully updated task"}, 200
  
     def patch(self):
-        todo_id = request.form.get("todo_id")
+        todo_id = request.form.get("todo_id") or request.form.get("todo_id")
         Todo.update_complete(todo_id)
         # return redirect(url_for("home"))
         return {"message": "successfully completed task"}, 200
 
     def delete(self):
-        todo_id = request.form.get("todo_id")
-        Todo.delete(todo_id)
+        todo_id = request.form.get("todo_id") or request.form.get("todo_id")
+        Todo.delete(todo_id) 
         # return redirect(url_for("home"))
         return {"message": "successfully deleted task"}, 200
