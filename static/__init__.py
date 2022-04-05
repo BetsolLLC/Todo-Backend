5af89399ed40b4c28bfd27a712632b3870dd7563
Ayush Gautam <Ayush.Gautam@betsol.com>
2022-04-05 19:20:11 +0530

    init

--- static/__init__.py
+++ static/__init__.py
@@ -10,11 +10,11 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/flasksql'
 
-    # from static.todoApp.model.todo_list_model import Todo
+    from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # from static.todoApp import todo_list
-    # app.register_blueprint(todo_list)
+    from static.todoApp import todo_list
+    app.register_blueprint(todo_list,url_prefix = "/api/v1")
 
     return app
--- static/todoApp/views/todo_list_view.py
+++ static/todoApp/views/todo_list_view.py
@@ -2,12 +2,9 @@ from flask import request, jsonify
 from flask_restful import Resource
 from flask_sqlalchemy import SQLAlchemy
 from static import db
-from static import cors
 from static.todoApp.model.todo_list_model import Todo
 from static.todoApp.utils.serialize_data import TodoListSerializer
-import logging
 
-logger = logging.getLogger(__name__)
 
 
 class TodoListView(Resource):
@@ -15,64 +12,34 @@ class TodoListView(Resource):
         return cors.before_request(self, *args, **kwargs)
 
     def get(self):
-        try:
-            serialize_instance = TodoListSerializer(Todo.get_all())
-            logger.debug(f"serialize_instance called here")
-            if serialize_instance.is_valid():
-                data = {
-                    "todo_list": serialize_instance.data(),
-                }
-                return jsonify(data), 200
-        except Exception as e:
-            logger.error(f"Error: {e}")
-            return {"message": "something went wrong"}, 500
+        serialize_instance = TodoListSerializer(Todo.get_all())
+        if serialize_instance.is_valid():
+            data = {
+                "todo_list": serialize_instance.data(),
+            }
+            return jsonify(data), 200
 
     def post(self):
-        try:
-            title = request.form.get("title")
-            Todo.create(title)
-            logger.info("New task created")
-
-            return {"message": "successfully added task"}, 201
-        except KeyError:
-            return {"message": "missing title"}, 400
-        except Exception as e:
-            logger.error(f"Error: {e}")
-            return {"message": "something went wrong"}, 500
+        title = request.form.get("title")
+        Todo.create(title)
+        # return redirect(url_for("home"))
+        return {"message": "successfully added task"}, 201
 
     def put(self):
-        try:
-            todo_id = request.form.get("todo_id")
-            title = request.form.get("title")
-            Todo.update(todo_id, title)
-
-            return {"message": "successfully updated task"}, 200
-        except KeyError:
-            return {"message": "missing title"}, 400
-        except ValueError as e:
-            logger.error(f"Error: {e}")
-            return {"message": "Todo with the request id not found"}, 400
+        todo_id = request.form.get("todo_id")
+        title = request.form.get("title")
+        Todo.update(todo_id,title)
+        # return redirect(url_for("home"))
+        return {"message": "successfully updated task"}, 200
  
     def patch(self):
-        try:
-            todo_id = request.form.get("todo_id")
-            Todo.update_complete(todo_id)
+        todo_id = request.form.get("todo_id")
+        Todo.update_complete(todo_id)
+        # return redirect(url_for("home"))
+        return {"message": "successfully completed task"}, 200
 
-            return {"message": "successfully completed task"}, 200
-        except KeyError:
-            return {"message": "Task not found"}, 400
-        except ValueError as e:
-            logger.error(f"Error: {e}")
-            return {"message": "Todo with the request id not found"}, 400
-        except Exception as e:
-            logger.error(f"Error: {e}")
-            return {"message": "something went wrong"}, 500
     def delete(self):
-        try:
-            todo_id = request.form.get("todo_id")
-            Todo.delete(todo_id)
-            return {"message": "successfully deleted task"}, 200
-        except KeyError:
-            return {"message": "task not found"}, 400
-        except ValueError as e:
-            return {"message": "Todo with the request id not found"}, 400
+        todo_id = request.form.get("todo_id")
+        Todo.delete(todo_id)
+        # return redirect(url_for("home"))
+        return {"message": "successfully deleted task"}, 200
