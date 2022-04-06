6be6ad58156557d515b036d6f80b6538f970603b
ayushgutambetsol <102720146+ayushgutambetsol@users.noreply.github.com>
2022-04-07 00:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 # Creating a Flask App
 app = Flask(__name__)
-
+CORS(app)
 
 # Connecting the Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zybrdrdsgxvtss:bd83da69e431203e28acfe61ffcdd891b303914f3cd6005058fff79f19a5abd6@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d5pn062kkjdnfu'
