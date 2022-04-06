749f5d4b4c213c7da513e58eaf01b1f9a569d31a
Ayush Gautam <Ayush.Gautam@betsol.com>
2022-04-06 13:03:39 +0530

    changes

--- static/__init__.py
+++ static/__init__.py
@@ -15,7 +15,7 @@ logger = custom_logger(logger)
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/SampleDb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://maepgdupzvcqru:2de24244b6ca685cd41dc98ae29c30c448ded6a6bcb0ff02afbd66dd5a35730f@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dbmevkl5rhd3tg'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from static.todoApp.model.todo_list_model import Todo
