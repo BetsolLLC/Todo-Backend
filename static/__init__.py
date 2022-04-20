b4d26157e20308723d025c049520f22cc770debc
Hitesh.Balegar <hitesh.balegar@betsol.com>
2022-04-20 10:56:09 +0530

    changed URI placeholder

--- static/__init__.py
+++ static/__init__.py
@@ -16,7 +16,7 @@ cors = CORS()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iifzaxouzngflp:072c008481eb606222b53e50bafae3d1890cc4aef01df54a47f537f4fc85bf41@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d4ormdorhkn74r'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://YOUR_URI'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
