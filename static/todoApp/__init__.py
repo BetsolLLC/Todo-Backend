acff226ebbc42da69d8d89916ec137b5554826dc
hitesh1balegar <102719066+hitesh1balegar@users.noreply.github.com>
2022-04-22 06:54:37 +0530

    Update __init__.py

--- static/todoApp/__init__.py
+++ static/todoApp/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 from .views.todo_list_view import TodoListView
 
 todo_list = Blueprint('todo', __name__)
-todo_list.add_url_rule('/', view_func=TodoListView.as_view('todo_list'), methods=['GET', 'POST', 'DELETE', 'PUT','PATCH'])
+todo_list.add_url_rule('/', view_func=TodoListView.as_view('todo_list'), methods=['GET', 'POST'])
 todo_list.add_url_rule('/<int:todo_id>', view_func=TodoListView.as_view('todo_list_id'), methods=['DELETE', 'PUT','PATCH'])
